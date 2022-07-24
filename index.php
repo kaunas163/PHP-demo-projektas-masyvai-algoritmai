634ab076cedf6e6d1cd8773b4ba37f51c24320a5
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2022-07-24 11:39:41 +0300

    2 - pasikuriam failus, pridedam pradinius stilius

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Automobiliai</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <div class="block">
+        <h1>Automobiliai</h1>
+        <p>Projektas apie automobilius.</p>
+    </div>
+
+</body>
+</html>
