fa0916f4d37de747192162b0e5aacaff9247eb56
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2022-07-24 12:20:05 +0300

    12 - informacijos susirinkimas is div atributu

--- scripts.js
+++ scripts.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelectorAll('.more-info-about-car').forEach(button => {
         button.addEventListener('click', function(event) {
-            console.log(event.target.parentElement);
+            let automobilis = {
+                marke: event.target.parentElement.attributes['data-marke'].value,
+                modelis: event.target.parentElement.attributes['data-modelis'].value,
+                rida: event.target.parentElement.attributes['data-rida'].value,
+                darbinis_turis: event.target.parentElement.attributes['data-darbinis_turis'].value,
+                gamybos_metai: event.target.parentElement.attributes['data-gamybos_metai'].value,
+                spalva: event.target.parentElement.attributes['data-spalva'].value,
+            };
+            console.log(automobilis);
         });
     });
 
