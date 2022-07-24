bae56af7853ec04a8798d7325e6e08f9e6b6dc4b
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2022-07-24 12:58:30 +0300

    24 - kiek kokiu skirtingu spalvu yra isvedimas

--- index.php
+++ index.php
@@ -335,6 +335,36 @@ foreach ($automobiliai as $auto) {
         ?>
     </div>
 
+    <div class="block">
+        <h2>Kiek kokių skirtingų spalvų automobilių yra</h2>
+        <?php
+        
+            if (count($skirtingos_spalvos) > 0) {
+                ?>
+
+                    <table border="1">
+                        <tr>
+                            <th>Spalva</th>
+                            <th>Atsikartojimai</th>
+                        </tr>
+                        <?php
+                            foreach ($skirtingos_spalvos as $spalva) {
+                                echo '<tr>';
+                                echo '<td>' . $spalva['spalva'] . '</td>';
+                                echo '<td>' . $spalva['atsikartojimai'] . '</td>';
+                                echo '</tr>';
+                            }
+                        ?>
+                    </table>
+
+                <?php
+            } else {
+                echo '<p>Skirtingų spalvų nebuvo rasta.</p>';
+            }
+        
+        ?>
+    </div>
+
     <div class="modal-wrapper">
         <div class="modal">
             <h2 class="title"></h2>
