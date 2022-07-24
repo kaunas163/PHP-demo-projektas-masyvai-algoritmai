af0af4e2ea357f0112f2d1154dd3349ca97f255e
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2022-07-24 13:04:22 +0300

    26 - naujausiu automobiliu isvedimas

--- index.php
+++ index.php
@@ -377,6 +377,47 @@ foreach ($automobiliai as $auto) {
         ?>
     </div>
 
+    <div class="block">
+        <h2>Automobiliai, kurių amžius yra 10 metų ar mažiau</h2>
+
+        <?php
+        
+            if (count($naujausi_automobiliai) > 0) {
+                ?>
+
+                    <table border="1">
+                        <tr>
+                            <th>Markė</th>
+                            <th>Modelis</th>
+                            <th>Gamybos metai</th>
+                            <th>Rida</th>
+                            <th>Darbinis tūris</th>
+                            <th>Spalva</th>
+                        </tr>
+                        <?php
+                            foreach ($naujausi_automobiliai as $auto) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $auto['marke']; ?></td>
+                                        <td><?php echo $auto['modelis']; ?></td>
+                                        <td><?php echo $auto['gamybos_metai']; ?></td>
+                                        <td><?php echo $auto['rida']; ?> km</td>
+                                        <td><?php echo $auto['darbinis_turis']; ?> l</td>
+                                        <td><?php echo $auto['spalva']; ?></td>
+                                    </tr>
+                                <?php
+                            }
+                        ?>
+                    </table>
+
+                <?php
+            } else {
+                echo '<p>Tokių automobilių nebuvo surasta.</p>';
+            }
+        
+        ?>
+    </div>
+
     <div class="modal-wrapper">
         <div class="modal">
             <h2 class="title"></h2>
