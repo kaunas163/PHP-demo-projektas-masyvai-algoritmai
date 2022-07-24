74110ee8ac728965a29156f1921084d8c53b0e36
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2022-07-24 11:48:23 +0300

    4 - susikuriam pradiniu duomenu masyva

--- index.php
+++ index.php
@@ -1,3 +1,74 @@
+<?php
+
+$automobiliai = array(
+    array(
+        'marke' => 'audi',
+        'modelis' => 'a4',
+        'rida' => 124587,
+        'darbinis_turis' => 1.6,
+        'gamybos_metai' => 2010,
+        'spalva' => 'pilka'
+    ),
+    array(
+        'marke' => 'tesla',
+        'modelis' => 's',
+        'rida' => 14578,
+        'darbinis_turis' => 1.2,
+        'gamybos_metai' => 2016,
+        'spalva' => 'raudona'
+    ),
+    array(
+        'marke' => 'citroen',
+        'modelis' => 'xsara',
+        'rida' => 149787,
+        'darbinis_turis' => 1.6,
+        'gamybos_metai' => 2005,
+        'spalva' => 'mėlyna'
+    ),
+    array(
+        'marke' => 'wolswagen',
+        'modelis' => 'polo',
+        'rida' => 427683,
+        'darbinis_turis' => 1.4,
+        'gamybos_metai' => 2004,
+        'spalva' => 'pilka'
+    ),
+    array(
+        'marke' => 'bmw',
+        'modelis' => 'e39',
+        'rida' => 349784,
+        'darbinis_turis' => 2.0,
+        'gamybos_metai' => 2002,
+        'spalva' => 'juoda'
+    ),
+    array(
+        'marke' => 'audi',
+        'modelis' => '80',
+        'rida' => 350497,
+        'darbinis_turis' => 1.6,
+        'gamybos_metai' => 1999,
+        'spalva' => 'juoda'
+    ),
+    array(
+        'marke' => 'hyundai',
+        'modelis' => 'i3',
+        'rida' => 41878,
+        'darbinis_turis' => 1.4,
+        'gamybos_metai' => 2012,
+        'spalva' => 'raudona'
+    ),
+    array(
+        'marke' => 'seat',
+        'modelis' => 'acorda',
+        'rida' => 4578,
+        'darbinis_turis' => 1.0,
+        'gamybos_metai' => 2022,
+        'spalva' => 'mėlyna'
+    )
+);
+
+?>
+
 <!DOCTYPE html>
 <html lang="lt">
 <head>
